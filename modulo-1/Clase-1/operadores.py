606e69cee7026e7a95e3de5004d218d60850e60e
Julian R <125599209+Navisper@users.noreply.github.com>
2025-06-02 22:09:01 -0500

    Add files via upload

---
+++ modulo-1/Clase-1/operadores.py
@@ -0,0 +1,30 @@
+'''
+#Declararacion y asignación de variables
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+
+#Implementar operadores 
+suma = num1 + num2
+resta = num1 - num2
+mult = num1 * num2
+div = num1/num2
+div2 = num1 // num2
+mod = num1 % num2
+pot = num1 ** num2
+
+#salida de datos por consola
+print(f"Suma: {suma}")
+print(f"Resta: {resta}")
+print(f"Multiplicación: {mult}")
+print(f"División: {div}")
+print(f"División entera: {div2}")
+print(f"Módulo: {mod}")
+print(f"Potencia: {pot}")
+'''
+x = 10
+x += 5  # Incrementa x en 5
+x -= 2  # Decrementa x en 2
+x *= 2  # Multiplica x por 2
+x /= 2 # Divide x entre 2
+
+print(x)
---
+++ modulo-1/Clase-1/tipos_de_datos.py
@@ -0,0 +1,9 @@
+#declaración de variables y solicitud de datos al usuario
+edad = int(input("¿Cuál es tu edad? "))
+salario = float(input("¿Cuál es tu salario? "))
+nombre = input("¿Cuál es tu nombre? ")
+
+#Salida de datos por consola
+print("la edad es: ", edad)
+print(f"el salario es: {salario:.2f}")
+print(f"el nombre es: {nombre}")
