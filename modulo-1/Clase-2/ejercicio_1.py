ed1b42adb7e26864a56903f832bdcb9baaa27844
Navisper <alobuenastarde@gmail.com>
2025-06-04 20:29:38 -0500

    updated exercise 1

--- modulo-1/Clase-2/ejercicio_1.py
+++ modulo-1/Clase-2/ejercicio_1.py
@@ -1,10 +1,17 @@
 #declarar y solicitar variables por consola
-print("----------TIENDA-----------")
-print("\nIngrese el precio de los productos")
+#Si solo compra el primer producto debe pagar el 100%
+#Si compra dos el 70%
+#Y si compra los tres es del 65%
+print("\n----------TIENDA-----------")
+print("\nIngrese el precio de los productos por favor :)\n")
 
 p1 = float(input("Precio producto 1 : "))
 p2 = float(input("Precio producto 2 : "))
 p3 = float(input("Precio producto 3 : "))
 
+total_a_pagar = p1 + p2 + p3
+
+total_con_descuento = total_a_pagar * 0.65
+
 print("\nGracias por confiar en nosotros!")
-print(f"El total a pagar es de: {p1 + p2 + p3}")
+print(f"El total a pagar es de: {total_a_pagar}\nPero como son 3 productos lleva un descuento del 35%\nSu total con descuento es : {total_con_descuento}")
