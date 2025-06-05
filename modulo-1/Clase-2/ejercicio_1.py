e276275c46710b346661fe90588171767342beb6
Navisper <alobuenastarde@gmail.com>
2025-06-04 20:18:13 -0500

    ejercicio 1 clase 2

---
+++ modulo-1/Clase-2/ejercicio_1.py
@@ -0,0 +1,10 @@
+#declarar y solicitar variables por consola
+print("----------TIENDA-----------")
+print("\nIngrese el precio de los productos")
+
+p1 = float(input("Precio producto 1 : "))
+p2 = float(input("Precio producto 2 : "))
+p3 = float(input("Precio producto 3 : "))
+
+print("\nGracias por confiar en nosotros!")
+print(f"El total a pagar es de: {p1 + p2 + p3}")
