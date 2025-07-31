8bd38307b1704d4c33281425129893eae84d99e7
Julian R <alobuenastarde@gmail.com>
2025-07-30 20:21:50 -0500

    Add example of list definition and iteration
    Created listas.py with code demonstrating how to define a list, assign values, and iterate through its elements, printing each color.

---
+++ modulo-3/clase-1/listas.py
@@ -0,0 +1,10 @@
+#definicion y carga de una lista 
+
+colores = []
+
+colores = ['rojo', 'verde', 'azul']
+
+print(colores)
+
+for color in colores:
+    print("los colores de la lista son: ", color)
